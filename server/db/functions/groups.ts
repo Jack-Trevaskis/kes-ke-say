20cfb5882c3eaa006640415726beb62018014112
jack-trevaskis <jack.trevaskis1@gmail.com>
2024-11-19 11:17:51 +1300

    get all routes

---
+++ server/db/functions/groups.ts
@@ -0,0 +1,26 @@
+import knexFile from '../knexfile.js'
+import knex from 'knex'
+
+type Environment = 'production' | 'test' | 'development';
+const environment = (process.env.NODE_ENV || 'development') as Environment;
+
+const config = knexFile[environment];
+export const db = knex(config);
+
+
+// - CRUD
+
+export async function getAllGroups () {
+
+    const groups = await db('groups')
+    console.log(groups)
+    return groups
+}
+
+// Create
+
+// Read
+
+// Update
+
+// Delete
--- server/routes/groups.ts
+++ server/routes/groups.ts
@@ -1,10 +1,21 @@
 import express from 'express'
+import * as db from '../db/functions/groups'
 
 const router = express.Router()
 
 // GET /api/v1/groups
-router.get('/', (req, res) => {
-  res.status(200).send('Hello from the groups route!')
+router.get('/', async (req, res) => {
+  // res.status(200).send('Hello from the groups route!')
+  try {
+  const groups = await db.getAllGroups()
+  console.log(groups)
+  
+    res.json(groups)
+  }
+
+  catch (err) {console.log('error')}
+  
+  
 })
 
 export default router
