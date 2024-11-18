a4fc1a2d5bf725eadadedf0122a71fa2dbd82ba0
Riley Wessel <lessewyendor@gmail.com>
2024-11-19 11:58:29 +1300

    fix getAllPosts return as postresponse

--- server/db/functions/posts.ts
+++ server/db/functions/posts.ts
@@ -1,7 +1,17 @@
 import db from '../connection'
 
 export async function getAllPosts() {
-  const result = await db('posts').select('*')
+  const result = await db('posts')
+    .join('users', 'posts.user_id', 'users.id')
+    .select(
+      'username as userName',
+      'users.image as userImage',
+      'auth0_id as userId',
+      'posts.id as postId',
+      'body as postBody',
+      'posts.image as postImage',
+      'created_at as postCreatedAt',
+    )
   // console.log(result)
   return result
 }
