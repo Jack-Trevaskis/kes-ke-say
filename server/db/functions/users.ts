3e00986af827bca96be23ed2024328bd98822659
kodi-dunn-manaia-24 <169906179+faelyn-jpg@users.noreply.github.com>
2024-11-19 10:54:43 +1300

    add getAllUsers() fn

---
+++ server/db/functions/users.ts
@@ -0,0 +1,27 @@
+import connection from '../connection.ts'
+import { User } from '../../../models/user'
+
+export async function getAllUsers(): Promise<User[]> {
+  return await connection('users')
+  .select('id',
+    'username', 
+    'location', 
+    'image',
+  )
+}
+
+//oops this is for next ticket
+
+// export async function getUsersById(id: number | string): Promise<User> {
+//   const user = await connection('users')
+//   .where({id})
+//   .select('id',
+//     'auth0_id as auth0Id', 
+//     'username', 
+//     'full_name as fullName', 
+//     'location', 
+//     'image',
+//   )
+//   .first()
+//   return user
+// }
