a4fc2886f0fb35cc68192d7851df9b10928fa8c2
Riley Wessel <lessewyendor@gmail.com>
2024-11-20 10:27:29 +1300

    fix api funcs

--- client/apis/posts.ts
+++ client/apis/posts.ts
@@ -1,7 +1,9 @@
 import request from 'superagent'
 import { Post, PostResponse } from '../../models/post'
 
+const rootURL = new URL(`/api/v1`, document.baseURI).toString()
+
 export async function getAllPosts(): Promise<PostResponse[]> {
-  const result = await request.get('/api/v1/posts/')
+  const result = await request.get(`${rootURL}/posts`)
   return result.body as PostResponse[]
 }
