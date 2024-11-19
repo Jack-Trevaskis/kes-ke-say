057f8df3c817540cd443a1dad64a1763ced6a95a
Riley Wessel <lessewyendor@gmail.com>
2024-11-20 11:51:12 +1300

    add getpostbyid function

--- server/db/functions/posts.ts
+++ server/db/functions/posts.ts
@@ -15,3 +15,22 @@ export async function getAllPosts() {
     )
   return result
 }
+
+export async function getPostById(ids: string) {
+  const id = Number(ids)
+  const result = await db('posts')
+    .where('posts.id', id)
+    .join('users', 'posts.user_id', 'users.id')
+    .select(
+      'users.username as userAccountName',
+      'users.full_name as userFullName',
+      'users.image as userImage',
+      'users.auth0_id as userId',
+      'posts.id as postId',
+      'posts.body as postBody',
+      'posts.image as postImage',
+      'posts.created_at as postCreatedAt',
+    )
+
+  return result
+}
