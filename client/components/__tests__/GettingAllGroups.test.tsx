166c409c516065f4eb505b721b9f40692d3429a6
DelphyM <dmladek@gmail.com>
2024-11-20 11:43:49 +1300

    Added a new test, but it's failing

--- client/components/__tests__/GettingAllGroups.test.tsx
+++ client/components/__tests__/GettingAllGroups.test.tsx
@@ -17,12 +17,7 @@ describe('All Groups', () => {
   it('it should display loading message', async () => {
     // ARRANGE
     // Mock the API endpoint
-    const scope = nock(document.baseURI)
-      .get('/api/v1/groups')
-      .reply(200, [
-        { id: 1, name: 'Group 1', image: 'exampleimage1.jpg' },
-        { id: 2, name: 'Group 2', image: 'exampleimage2.png' },
-      ])
+    const scope = nock(document.baseURI).get('/api/v1/groups').reply(200)
 
     // ACT
     // Render the component
@@ -30,9 +25,31 @@ describe('All Groups', () => {
 
     const loadingMessage = await screen.getByText(/loading/i) // 'loading'
     expect(loadingMessage).toBeInTheDocument()
+  })
 
-    // const name = await screen.findByText('Group 1')
-    // expect(name).toBeVisible()
-    // expect(scope.isDone()).toBe(true)
+  it('it should display the groups', async () => {
+    // ARRANGE
+    // Mock the API endpoint
+    const scope = nock(document.baseURI)
+      .get('/api/v1/groups')
+      .reply(200, [
+        { id: 1, name: 'Group 1', image: 'exampleimage1.jpg' },
+        { id: 2, name: 'Group 2', image: 'exampleimage2.png' },
+      ])
+    // ACT
+    // Render the component
+    const { ...screen } = renderRoute('/groups')
+
+    // ASSERT
+    // Check if the loading message is not displayed
+    await waitForElementToBeRemoved(() => screen.getByText(/loading/i))
+
+    // Check if the groups are displayed
+    const name1 = await screen.findByText('Group 1')
+    expect(name1).toBeVisible()
+    const name2 = await screen.findByText('Group 2')
+    expect(name2).toBeVisible()
+    // Check if the API call was made
+    expect(scope.isDone()).toBe(true)
   })
 })
