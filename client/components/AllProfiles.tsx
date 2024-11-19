8242af32136368ead017d627d62e4466b8b98fed
kodi-dunn-manaia-24 <169906179+faelyn-jpg@users.noreply.github.com>
2024-11-20 11:07:13 +1300

    add key to profiles list

--- client/components/AllProfiles.tsx
+++ client/components/AllProfiles.tsx
@@ -12,8 +12,8 @@ export default function AllProfiles() {
     <div className="text-red-950">
     <h2 className="text-center mt-4 mb-4">All Profiles</h2>
         <div className="UserProfilesList text-white flex justify-around flex-wrap">
-          {users?.map((u) => (<><div className="profile text-center justify-center text-red-950">
-              <Link to={`/profiles/${u.username}`}><img alt="" className="max-w-48 max-h-48 border-4 rounded-full border-red-950" src={`../images/avatars/${u.image}`}/>{u.username}</Link></div></>))}           
+          {users?.map((u) => (<div key={u.id}><div  className="profile text-center justify-center text-red-950">
+              <Link to={`/profiles/${u.username}`}><img alt="" className="max-w-48 max-h-48 border-4 rounded-full border-red-950" src={`../images/avatars/${u.image}`}/>{u.username}</Link></div></div>))}           
         </div>
         </div>
     )
