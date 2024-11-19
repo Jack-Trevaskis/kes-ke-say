44af3b7bcf909574855c3ce85bc6b6ff79f83e59
zak-hynd <119102024+zak-hynd@users.noreply.github.com>
2024-11-19 16:52:44 +1300

    initialise placeholder Profile component page

--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -1,10 +1,10 @@
-
+//placeholder, this component doesn't need to be filled in until the next ticket
 
 export  default function Profile () {
 return (
     <>
     <div className="profile bg-gray-200 text-white">
-        <div className="profileName">ProfileNameGoesHere</div>
+        <div className="profileName">ProfileNameGoesHere: Ida dia</div>
     </div>
     </>
 )
