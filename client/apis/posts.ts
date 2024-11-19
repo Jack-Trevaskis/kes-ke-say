b3d94f8b6601f089b8d82d87316ee2e76b8a2246
Riley Wessel <lessewyendor@gmail.com>
2024-11-20 12:10:04 +1300

    add getpostbyid api function

--- client/apis/posts.ts
+++ client/apis/posts.ts
@@ -1,9 +1,14 @@
 import request from 'superagent'
-import { Post, PostResponse } from '../../models/post'
+import { PostResponse } from '../../models/post'
 
 const rootURL = new URL(`/api/v1`, document.baseURI).toString()
 
 export async function getAllPosts(): Promise<PostResponse[]> {
   const result = await request.get(`${rootURL}/posts`)
   return result.body as PostResponse[]
+}
+
+export async function getPostById(id: number): Promise<PostResponse> {
+  const result = await request.get(`${rootURL}/posts/${id}`)
+  return result.body as PostResponse
 }
