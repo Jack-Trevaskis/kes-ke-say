4d75825d8061e692f2e17ac5e2598c4836e237c5
kodi-dunn-manaia-24 <169906179+faelyn-jpg@users.noreply.github.com>
2024-11-19 16:54:28 +1300

    add styling to AllProfiles page

--- client/components/AllProfiles.tsx
+++ client/components/AllProfiles.tsx
@@ -9,9 +9,9 @@ export default function AllProfiles() {
   if(isError) return <h2>An error has occurred loading users.</h2>
     
     return (<>
-    <h2>All Profiles</h2>
-        <div className="UserProfilesList bg-black text-white flex">
-          {users?.map((u) => (<><div className="profile flex-wrap"><li key={u.username}>
+    <h2 className="text-center">All Profiles</h2>
+        <div className="UserProfilesList bg-black text-white flex justify-around">
+          {users?.map((u) => (<><div className="profile flex-wrap text-center"><li key={u.username}>
               <Link to={`/profiles/${u.username}`}><img alt="" src={`../images/avatars/${u.image}`}/>{u.username}</Link></li></div></>))}           
         </div></>
     )
