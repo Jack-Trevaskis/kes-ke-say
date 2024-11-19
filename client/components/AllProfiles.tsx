13b92d06b40fa655de20ec66dd39f2ee3d853dc2
kodi-dunn-manaia-24 <169906179+faelyn-jpg@users.noreply.github.com>
2024-11-19 16:51:20 +1300

    initialise AllProfiles component and render avatar and username

--- client/components/AllProfiles.tsx
+++ client/components/AllProfiles.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom"
 import { useUsers } from "../hooks/useUsers"
+
 
 
 export default function AllProfiles() {
@@ -6,7 +8,11 @@ export default function AllProfiles() {
   if(isLoading) return "Loading..."
   if(isError) return <h2>An error has occurred loading users.</h2>
     
-    return (
-        <div className="UserProfilesList bg-black text-white">blabla bla</div>
+    return (<>
+    <h2>All Profiles</h2>
+        <div className="UserProfilesList bg-black text-white flex">
+          {users?.map((u) => (<><div className="profile flex-wrap"><li key={u.username}>
+              <Link to={`/profiles/${u.username}`}><img alt="" src={`../images/avatars/${u.image}`}/>{u.username}</Link></li></div></>))}           
+        </div></>
     )
 }
