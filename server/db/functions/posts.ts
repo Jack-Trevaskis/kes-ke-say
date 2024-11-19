f6c65cbab39925d93da2f0179f4cb8f531b89b89
Aleksei Stukalov <stukalov.nz@gmail.com>
2024-11-19 17:00:45 +1300

    format db response to return data based on new model updates

--- server/db/functions/posts.ts
+++ server/db/functions/posts.ts
@@ -4,14 +4,14 @@ export async function getAllPosts() {
   const result = await db('posts')
     .join('users', 'posts.user_id', 'users.id')
     .select(
-      'username as userName',
+      'users.username as userAccountName',
+      'users.full_name as userFullName',
       'users.image as userImage',
-      'auth0_id as userId',
+      'users.auth0_id as userId',
       'posts.id as postId',
-      'body as postBody',
+      'posts.body as postBody',
       'posts.image as postImage',
-      'created_at as postCreatedAt',
+      'posts.created_at as postCreatedAt',
     )
-  // console.log(result)
   return result
 }
