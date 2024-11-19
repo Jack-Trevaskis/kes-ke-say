5ec5ee2a8ea685f8ce7bdf98ab4bed4603295864
Aleksei Stukalov <stukalov.nz@gmail.com>
2024-11-19 16:56:50 +1300

    remove redundant code

--- client/hooks/posthooks.ts
+++ client/hooks/posthooks.ts
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query'
-import request from 'superagent'
 import * as api from '../apis/posts'
 
 export function usePosts() {
@@ -7,6 +6,5 @@ export function usePosts() {
     queryKey: ['posts'],
     queryFn: () => api.getAllPosts(),
   })
-  console.log('getAllPosts', query)
   return query
 }
