d8878a97b9dfe8089747bf65fa7be8567a56b534
Aleksei Stukalov <stukalov.nz@gmail.com>
2024-11-20 11:34:12 +1300

    add post-feed component tests to show data appropriately

--- client/components/__tests__/LandingPage.test.tsx
+++ client/components/__tests__/LandingPage.test.tsx
@@ -1,6 +1,6 @@
 // @vitest-environment jsdom
 import { describe, it, expect } from 'vitest'
-import { screen, waitForElementToBeRemoved } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import { renderRoute } from '../../test-utils'
 
@@ -12,27 +12,5 @@ describe('<LandingPage />', () => {
     expect(button).toContainHTML('button')
     expect(button).toHaveClass('px-8')
     expect.assertions(2)
-  })
-})
-
-describe('<Home />', () => {
-  it('renders a main tag', () => {
-    renderRoute('/')
-
-    const main = screen.getByRole('main')
-    expect(main).toContainHTML('main')
-    expect.assertions(1)
-  })
-
-  it('renders a post feed section', async () => {
-    renderRoute('/')
-
-    // const loading = screen.getByText('Loading...')
-    await waitForElementToBeRemoved(async () => {
-      const section = screen.getByTestId('post-feed')
-      expect(section).toContainHTML('section')
-    })
-
-    expect.assertions(1)
   })
 })
