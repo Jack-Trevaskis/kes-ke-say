101b0cd018c542175e2eb85c63f4e89a82167c86
Riley Wessel <lessewyendor@gmail.com>
2024-11-20 14:30:24 +1300

    add db test

--- server/db/__tests__/posts.test.ts
+++ server/db/__tests__/posts.test.ts
@@ -12,11 +12,19 @@ beforeEach(async () => {
   await db.seed.run()
 })
 
-describe('Read functions', () => {
+describe('getAllPosts', () => {
   it('should return a PostResponse array of 4 objects', async () => {
     const posts: PostResponse[] = await func.getAllPosts()
 
     expect(posts).toHaveLength(4)
     expect(posts[1].userId).toBe('auth0|234')
+  })
+})
+
+describe('getPostById', () => {
+  it('should return a single PostResponse', async () => {
+    const post: PostResponse = await func.getPostById('1')
+
+    expect(post.userId).toBe('auth0|123')
   })
 })
