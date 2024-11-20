04e41bab7c21162a872c1376e92715bf9b67b0c8
DelphyM <dmladek@gmail.com>
2024-11-20 16:03:26 +1300

    Converted DIV to BUTTON

--- client/components/AllGroups.tsx
+++ client/components/AllGroups.tsx
@@ -36,7 +36,7 @@ export function AllGroups() {
       >
         {groups.length > 0 ? (
           groups.map((group: Group) => (
-            <div
+            <button
               key={group.id}
               onClick={() => navigate('/groups/' + group.id)}
               style={{
@@ -72,7 +72,7 @@ export function AllGroups() {
               >
                 {group.name}
               </div>
-            </div>
+            </button>
           ))
         ) : (
           <div style={{ textAlign: 'center', color: '#555' }}>
